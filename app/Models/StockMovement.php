432beae43f77d8992a1e10d6a3238786b0648932
utsav07uv <utsav.rai.uv07@gmail.com>
2025-09-22 10:18:00 +0000

    stock record scope

--- app/Models/StockMovement.php
+++ app/Models/StockMovement.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use App\Enums\StockMovementType;
+use App\Models\Scopes\StockMovementScope;
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Model;
 
+#[ScopedBy(StockMovementScope::class)]
 class StockMovement extends Model
 {
     protected $fillable = [
